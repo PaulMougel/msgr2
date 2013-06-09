99b29f00516998ea47f80f6dfa38c4cfe7c957a5
Guillaume Burel <guillaume@yimgo.fr>
2013-06-09 11:37:45 +0200

    Implement OPML import.

--- src/app/scripts/services/omplService.js
+++ src/app/scripts/services/omplService.js
@@ -2,13 +2,20 @@
 
 angular.module('msgr')
   .factory('omplService', function ($rootScope, $http, newsblurService) {
-    // Service logic
-    // ...
-
     // Public API here
     return {
-      import: function () {
-        return meaningOfLife;
+      import: function (opml) {
+        parser = new DOMParser();
+        xmlDoc = parser.parseFromString(opml,"text/xml");
+        outlines = xmlDoc.getElementsByTagName("outline");
+        var feeds, i;
+        for (i = 0; i < outlines.length; i += 1) {
+            if (outlines[i].attributes.xmlUrl && outlines[i].attributes.htmlUrl && outlines[i].attributes.type.nodeValue === "rss") { // is a feed
+                console.log(">>> [" + outlines[i].attributes.title.nodeValue + "](" + outlines[i].attributes.xmlUrl.nodeValue + ")");
+            } else { // is a folder
+                console.log(outlines[i].attributes.title.nodeValue);
+            }
+        }
       }
     };
   });
