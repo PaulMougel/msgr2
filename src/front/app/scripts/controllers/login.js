5748a42adcbb5e7a6a4d99d53e355350c5187ac0
Paul Mougel <mougelpaul@gmail.com>
2013-07-05 17:28:59 +0200

    After a successful sign up, the frontend will automatically sign in the user
    fix #17

--- src/front/app/scripts/controllers/login.js
+++ src/front/app/scripts/controllers/login.js
@@ -2,9 +2,7 @@
 
 angular.module('msgr')
     .controller('LoginController', function ($scope, $location, authService) {
-        $scope.signin = function() {
-            var user = { login: $scope.login, password: $scope.password };
-
+        var signin = function(user) {
             authService.signin(user)
             .success(function() {
                 $location.path('/subscriptions/');
@@ -14,12 +12,16 @@ angular.module('msgr')
             });
         };
 
+        $scope.signin = function() {
+            signin({ login: $scope.login, password: $scope.password });
+        };
+
         $scope.signup = function() {
             var user = { login: $scope.login, password: $scope.password };
             
             authService.signup(user)
             .success(function() {
-                $location.path('/subscriptions/');
+                return signin(user);
             })
             .error(function() {
                 alert('Signup failure!');
